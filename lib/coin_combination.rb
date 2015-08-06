d792cc488d5470d42a167a2b97ec27e02b11d847
Travis Johnston & Tim Kellogg <student@epicodus.com>
2015-08-06 10:09:42 -0700

    handles quarters

--- lib/coin_combination.rb
+++ lib/coin_combination.rb
@@ -1,4 +1,11 @@
 define_method(:coin_combination) do |change|
 
+  total = change
+  returned_change = []
+
+  quarters = (total / 25).floor
+  remaining_change = total % 25
+
+  return [quarters]
 
 end
